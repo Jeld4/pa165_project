38671f5fbea3568fe7e05eb86379f28ee1d1550f
Michal Klíma <Michal Klma@192.168.0.80>
2020-03-28 16:42:16 +0100

    added javadoc to user dao

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,22 +1,74 @@
-package dao;
+package cz.fi.muni.pa165.dao;
 
 import java.util.List;
 
-import entity.Customer;
-import entity.Employee;
-import entity.User;
+import cz.fi.muni.pa165.entity.Customer;
+import cz.fi.muni.pa165.entity.Employee;
+import cz.fi.muni.pa165.entity.User;
+
+/**
+ * @author Michal Klíma
+ */
 
 public interface UserDao {
+	
+    /**
+     * Method to create a new Customer.
+     * @param customer
+     */
 	void createCustomer(Customer customer);
+	
+    /**
+     * Method to create a new Employee.
+     * @param employee
+     */
 	void createEmployee(Employee employee);
 	
+    /**
+     * Method to create a new User.
+     * @param employee
+     */
+	void createUser(User user);
+	
+	
+    /**
+     * Method to find a Customer by id.
+     * @param id
+     */
 	Customer findCustomer(long id);
+	
+    /**
+     * Method to find a Employee by id.
+     * @param id
+     */
 	Employee findEmployee(long id);
+	
+    /**
+     * Method to find a User by id.
+     * @param id
+     */
 	User findUser(long id);
 	
-	void removeUser(long id);
 	
-	List<Customer> getAllCustomers();
-	List<Employee> getAllEmployees();
-	List<User> getAllUsers();
+    /**
+     * Method to remove a User.
+     * @param user
+     */
+	void removeUser(User user);
+	
+	
+    /**
+     * Method to find all Customers
+     */
+	List<Customer> findAllCustomers();
+	 
+	/**
+     * Method to find all Employees
+     */
+	List<Employee> findAllEmployees();
+	
+	 /**
+     * Method to find all Users
+     */
+	List<User> findAllUsers();
 }
--- src/main/java/dao/UserDaoImpl.java
+++ src/main/java/dao/UserDaoImpl.java
@@ -1,63 +1,77 @@
-package dao;
+package cz.fi.muni.pa165.dao;
 
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
-import entity.Customer;
-import entity.Employee;
-import entity.User;
+import cz.fi.muni.pa165.entity.Customer;
+import cz.fi.muni.pa165.entity.Employee;
+import cz.fi.muni.pa165.entity.Order;
+import cz.fi.muni.pa165.entity.User;
+import org.springframework.stereotype.Repository;
 
+/**
+ * @author Michal Klíma
+ */
+
+@Repository
 public class UserDaoImpl implements UserDao {
     @PersistenceContext
     private EntityManager em;
 
-    public void setEntityManager(EntityManager e) {
-    	em =e;
-    }
-    
+    @Override
 	public void createCustomer(Customer customer) {
 		em.persist(customer);
 	}
 
+    @Override
 	public void createEmployee(Employee employee) {
 		em.persist(employee);
 	}
+    
+    @Override
+	public void createUser(User user) {
+		em.persist(user);
+	}
 
+    @Override
 	public Customer findCustomer(long id) {
 		return em.find(Customer.class, id);
 	}
 
+    @Override
 	public Employee findEmployee(long id) {
 		return em.find(Employee.class, id);
 	}
 	
+    @Override
 	public User findUser(long id) {
 		return em.find(User.class, id);
 	}
-
-	public void removeUser(long id) {
-		em.remove(findUser(id));
-	}
 	
+    @Override
 	public void removeUser(User user) {
 		em.remove(user);
 	}
 
-	public List<Customer> getAllCustomers() {
-		// TODO Auto-generated method stub
-		return null;
+    @Override
+	public List<Customer> findAllCustomers() {
+        Query query = em.createQuery("SELECT c FROM Customer c");
+        return (List<Customer>) query.getResultList();
 	}
 
-	public List<Employee> getAllEmployees() {
-		// TODO Auto-generated method stub
-		return null;
+    @Override
+	public List<Employee> findAllEmployees() {
+        Query query = em.createQuery("SELECT e FROM Employee e");
+        return (List<Employee>) query.getResultList();
 	}
 
-	public List<User> getAllUsers() {
-		// TODO Auto-generated method stub
-		return null;
+    @Override
+	public List<User> findAllUsers() {
+        Query query = em.createQuery("SELECT u FROM Users u");
+        return (List<User>) query.getResultList();
 	}
 
 }
