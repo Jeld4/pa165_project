bcce97ecb8a7360d1edf68e27dc22bebd09604ba
Michal Klíma <456234@mail.muni.cz>
2020-03-29 16:01:21 +0200

    remove comments

--- src/test/java/cz/fi/muni/pa165/dao/UserDaoImplTest.java
+++ src/test/java/cz/fi/muni/pa165/dao/UserDaoImplTest.java
@@ -19,47 +19,47 @@ import org.testng.annotations.Test;
 @TestExecutionListeners(TransactionalTestExecutionListener.class)
 @Transactional
 public class UserDaoImplTest extends AbstractTestNGSpringContextTests {
-    // 
-    // @Autowired
-    // private UserDao userDao;
-    //
-    // private User u1;
-    // private User u2;
-    //
-    // @BeforeMethod
-    // public void prepare(){
-    //     u1 = new User();
-    //     u2 = new User();
-    //
-    //     u1.setLogin("xlogin1");
-    //     u2.setLogin("xlogin2");
-    //
-    //     u1.setName("user1");
-    //     u2.setName("user2");
-    //
-    //     userDao.createUser(u1);
-    //     userDao.createUser(u2);
-    // }
-    //
-    // @Test
-    // public void testFindUser(){
-    //     Assert.assertEquals(userDao.findUser(u1.getId()), u1);
-    // }
-    // @Test
-    // public void testFindAll(){
-    //     Assert.assertEquals(userDao.findAllUsers().size(), 2);
-    // }
-    // @Test
-    // public void testRemove(){
-    //     userDao.removeUser(u1);
-    //     Assert.assertNull(userDao.findUser(u1.getId()));
-    //     Assert.assertEquals(userDao.findAllUsers().size(), 1);
-    // }
-    //
-    // @Test
-    // public void updateUser(){
-    //     u1.setLogin("newLogin1");
-    //     //TODO
-    // }
+     
+     @Autowired
+     private UserDao userDao;
+    
+     private User u1;
+     private User u2;
+    
+     @BeforeMethod
+     public void prepare(){
+         u1 = new User();
+         u2 = new User();
+    
+         u1.setLogin("xlogin1");
+         u2.setLogin("xlogin2");
+    
+         u1.setName("user1");
+         u2.setName("user2");
+    
+         userDao.createUser(u1);
+         userDao.createUser(u2);
+     }
+    
+     @Test
+     public void testFindUser(){
+         Assert.assertEquals(userDao.findUser(u1.getId()), u1);
+     }
+     @Test
+     public void testFindAll(){
+         Assert.assertEquals(userDao.findAllUsers().size(), 2);
+     }
+     @Test
+     public void testRemove(){
+         userDao.removeUser(u1);
+         Assert.assertNull(userDao.findUser(u1.getId()));
+         Assert.assertEquals(userDao.findAllUsers().size(), 1);
+     }
+    
+     @Test
+     public void updateUser(){
+         u1.setLogin("newLogin1");
+         //TODO
+     }
 
 }
