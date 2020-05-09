cacbc61a475cff283b6081b0e658203d80dfba3a
Jakub Malý <xmaly@fi.muni.cz>
2020-05-09 13:06:41 +0200

    User.equals fix

--- persistence/src/main/java/cz/fi/muni/pa165/entity/User.java
+++ persistence/src/main/java/cz/fi/muni/pa165/entity/User.java
@@ -1,6 +1,7 @@
 package cz.fi.muni.pa165.entity;
 
 import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -85,51 +86,20 @@ public class User {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((isAdmin == null) ? 0 : isAdmin.hashCode());
-		result = prime * result + ((login == null) ? 0 : login.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((password == null) ? 0 : password.hashCode());
-		return result;
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		User user = (User) o;
+		return Objects.equals(id, user.id) &&
+				Objects.equals(name, user.name) &&
+				Objects.equals(login, user.login) &&
+				Objects.equals(password, user.password) &&
+				Objects.equals(isAdmin, user.isAdmin) &&
+				Objects.equals(orders, user.orders);
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (isAdmin == null) {
-			if (other.isAdmin != null)
-				return false;
-		} else if (!isAdmin.equals(other.isAdmin))
-			return false;
-		if (login == null) {
-			if (other.login != null)
-				return false;
-		} else if (!login.equals(other.login))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (password == null) {
-			if (other.password != null)
-				return false;
-		} else if (!password.equals(other.password))
-			return false;
-		return true;
+	public int hashCode() {
+		return Objects.hash(id, name, login, password, isAdmin, orders);
 	}
 }
